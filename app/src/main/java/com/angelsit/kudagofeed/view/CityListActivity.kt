63cd89adc74f267e2266ab5b26be04d31bdc8a7e
vova <vovanychkov95@gmail.com>
2019-03-11 08:38:14 +0300

    city activity created

---
+++ app/src/main/java/com/angelsit/kudagofeed/view/CityListActivity.kt
@@ -0,0 +1,14 @@
+package com.angelsit.kudagofeed.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.angelsit.kudagofeed.R
+
+class CityListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_list)
+
+    }
+}
